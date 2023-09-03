c91a6e8d18fdcae97dc9b2f4f3c76bf1d155c24d
Arman Azarnik <armanazarnik@gmail.com>
2023-09-03 11:50:17 +0330

    add contact info v 2.1

--- Fibonacci loop.py
+++ Fibonacci loop.py
@@ -4,18 +4,23 @@ def main():
     while(True):
         print("Please enter the number of fibonacci series :")
         numberOfSeries=int(input())
-        print("************")
-        first = 0
-        second = 1
-        for i in range(1, numberOfSeries+1):
-            print(first)
-            third = first+second
-            first = second
-            second = third
+        print("******************")
+        fibonacciSerieLoop(numberOfSeries)
+
+
+def fibonacciSerieLoop(n):
+    first = 0
+    second = 1
+    for i in range(1, n+1):
+        print(first)
+        third = first+second
+        first = second
+        second = third
 
 
 if __name__ == '__main__':
     main()
 
 
-#by Arman Azarnik
+# by Arman Azarnik
+# armanazarnik@gmail.com
